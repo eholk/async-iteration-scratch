528a4c7c040c698ab0efd241683136b8d0200e5c
Eric Holk <ericholk@microsoft.com>
2024-07-25 13:11:59 -0700

    add AFIT map

---
+++ src/afit/map.rs
@@ -0,0 +1,59 @@
+use super::AsyncIterator;
+
+pub fn map<Iter: AsyncIterator, F: async Fn(Iter::Item) -> U, U>(
+    iter: Iter,
+    f: F,
+) -> Map<Iter, F, U> {
+    Map { iter, f }
+}
+
+pub struct Map<Iter: AsyncIterator, F, U>
+where
+    F: async Fn(Iter::Item) -> U,
+{
+    iter: Iter,
+    f: F,
+}
+
+impl<Iter, F, U> AsyncIterator for Map<Iter, F, U>
+where
+    Iter: AsyncIterator,
+    F: async Fn(Iter::Item) -> U,
+{
+    type Item = U;
+
+    async fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next().await {
+            Some(item) => Some((self.f)(item).await),
+            None => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::iter;
+
+    use crate::{
+        afit::{async_iter_from_iter, map::map, AsyncIterator},
+        block_on,
+    };
+
+    #[test]
+    fn simple_map() {
+        block_on(async {
+            let iter = async_iter_from_iter(0..3);
+            let mut result = vec![];
+
+            let mapped = map(iter, async |x| x + 1);
+
+            mapped
+                .for_each(async |x| {
+                    result.push(x);
+                })
+                .await;
+
+            assert_eq!(result, vec![1, 2, 3]);
+        })
+    }
+}
---
+++ src/afit/mod.rs
@@ -0,0 +1,38 @@
+use std::pin::Pin;
+
+mod map;
+
+pub trait AsyncIterator {
+    type Item;
+
+    async fn next(&mut self) -> Option<Self::Item>;
+
+    async fn for_each(mut self, mut f: impl async FnMut(Self::Item))
+    where
+        Self: Sized,
+    {
+        while let Some(item) = self.next().await {
+            f(item).await;
+        }
+    }
+}
+
+pub trait PinnedAsyncIterator {
+    type Item;
+
+    async fn next(self: Pin<&mut Self>) -> Option<Self::Item>;
+}
+
+fn async_iter_from_iter<I: Iterator>(iter: I) -> impl AsyncIterator<Item = I::Item> {
+    struct Iter<I: Iterator>(I);
+
+    impl<I: Iterator> AsyncIterator for Iter<I> {
+        type Item = I::Item;
+
+        async fn next(&mut self) -> Option<Self::Item> {
+            self.0.next()
+        }
+    }
+
+    Iter(iter)
+}
